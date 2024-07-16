eb66e7ebb5755547e869bae0029acca9e4333b2f
Joselito Guardado <jaguardado123@gmail.com>
2024-07-16 00:16:14 -0500

    First commit

---
+++ src/AppTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class AppTest {
+	
+	@Test
+	//@DisplayName("Testing App function")
+	public void testApp() {
+		assertEquals(0, 5 - 5);
+	}
+}
